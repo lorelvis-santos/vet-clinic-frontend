f4fe0d8968848a40f669a16cc4463db08582501a
lorelvis-santos <santoslorelvis@gmail.com>
2025-10-25 09:31:55 -0400

    refactor: restructure App component and simplify main rendering logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-function Home() {
-  return (
-    <>
-      <h1 className="font-bold text-4xl">Inicio</h1>
-    </>
-  )
+import { BrowserRouter, Routes, Route } from "react-router";
+import AuthLayout from './layout/AuthLayout.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import ForgotPassword from './pages/ForgotPassword.jsx';
+import VerifyAccount from './pages/VerifyAccount.jsx';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<AuthLayout />}>
+                    <Route index element={<Login />} /> 
+                    <Route path="registrar" element={<Register />} />
+                    <Route path="confirmar-cuenta/:token" element={<VerifyAccount />} />
+                    <Route path="recuperar-contraseña" element={<ForgotPassword />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default Home
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
-import './index.css'
-import AuthLayout from './layout/AuthLayout.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import ForgotPassword from './pages/ForgotPassword.jsx';
-import VerifyAccount from './pages/VerifyAccount.jsx';
 import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx';
 
 const root = document.getElementById("root");
 
+// Forma de ver los env en VITE
+// console.log(import.meta.env.VITE_BACKEND_URL)
+
 createRoot(root).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<AuthLayout />}>
-                    <Route index element={<Login />} /> 
-                    <Route path="registrar" element={<Register />} />
-                    <Route path="confirmar-cuenta/:token" element={<VerifyAccount />} />
-                    <Route path="recuperar-contraseña" element={<ForgotPassword />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
+    // <StrictMode>
+        <App/>
+    // </StrictMode>
 )
